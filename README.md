10b0aabf88ca30face7c5586406dff4f58575a17
mrpedrobraga <mrhenribraga@gmail.com>
2023-04-11 09:56:48 -0300

    Added more boilerplate.

---
+++ README.md
@@ -0,0 +1,17 @@
+# My Plugin
+
+## Description
+
+It does this and this and that.
+
+## Features
+
+- A
+- B
+- C
+
+## Roadmap
+
+- A
+- B
+- C
---
+++ addons/my_addon/plugin.gd
@@ -0,0 +1,10 @@
+@tool
+extends EditorPlugin
+
+var editor_interface := get_editor_interface()
+
+func _enter_tree():
+	pass
+
+func _exit_tree():
+	pass
